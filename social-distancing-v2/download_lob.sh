0a6e26d62719e0f5235fee93bea7a5adbbc7014a
Maksim Pischulenok <pischule@outlook.com>
2022-03-14 11:53:26 +0300

    add download script

---
+++ social-distancing-v2/download_lob.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+wget -nc --output-document video/vid0.mp4 https://nextcloud.pischulenok.xyz/s/iTpNXgwZbfEA4Ws/download?path=&files=vid0.mp4
+wget -nc --output-document video/vid1.mp4 https://nextcloud.pischulenok.xyz/s/iTpNXgwZbfEA4Ws/download?path=&files=vid1.mp4
+
+wget -nc --output-document models/yolov3_tiny/n.weights https://nextcloud.pischulenok.xyz/s/iTpNXgwZbfEA4Ws/download?path=&files=yolov3_tiny.mp4
+wget -nc --output-document models/yolov3/n.weights https://nextcloud.pischulenok.xyz/s/iTpNXgwZbfEA4Ws/download?path=&files=yolov3.mp4
