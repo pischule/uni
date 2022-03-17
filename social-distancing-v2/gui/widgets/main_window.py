b1d88a249fbc0c11ba9f0cde663d02d92baf0c8c
Maksim Pischulenok <pischule@outlook.com>
2022-03-18 01:48:11 +0300

    add persistent settings

--- social-distancing-v2/gui/widgets/main_window.py
+++ social-distancing-v2/gui/widgets/main_window.py
@@ -1,3 +1,5 @@
+import dataclasses
+import json
 import sys
 
 import PySide6
@@ -20,20 +22,25 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.camThread.start()
         self.setupUi(self)
 
-        self._cameras = [
-            # ("0",),
-            # ('../../video/vid0.mp4',),
-            # ('../../video/vid1.mp4',),
-            # ('../../video/vid2.avi',),
-            ('../../video/vid2.avi',
-             1,
-             [(-132.49023090586147, 173.9253996447602), (-47.57371225577265, 206.66429840142095),
-              (-2.0461811722912966, 162.15985790408524), (-74.1740674955595, 145.27886323268206)],
-             [(-172.38336347197108, 69.7866184448463), (-86.45207956600362, 48.95479204339964),
-              (13.01989150090416, 55.725135623869804), (12.499095840867994, 146.86437613019893),
-              (-172.38336347197108, 147.90596745027125)])
-        ]
-        print(Camera.from_wizard(*self._cameras[0][1::]))
+        self._cameras = []
+        with open('cameras2.json', 'r') as f:
+            self._cameras = [Camera(**c) for c in json.load(f)]
+
+        # self._cameras = [
+        #     # ("0",),
+        #     # ('../../video/vid0.mp4',),
+        #     # ('../../video/vid1.mp4',),
+        #     # ('../../video/vid2.avi',),
+        #     Camera.from_wizard("../../video/vid2.avi", 100,
+        #                        [(-132.49023090586147, 173.9253996447602), (-47.57371225577265, 206.66429840142095),
+        #                         (-2.0461811722912966, 162.15985790408524), (-74.1740674955595, 145.27886323268206)],
+        #                        [(-172.38336347197108, 69.7866184448463), (-86.45207956600362, 48.95479204339964),
+        #                         (13.01989150090416, 55.725135623869804), (12.499095840867994, 146.86437613019893),
+        #                         (-172.38336347197108, 147.90596745027125)])
+        # ]
+
+        self.cameraComboBox.currentIndexChanged.connect(self.switch_camera)
+        self.addCameraPushButton.clicked.connect(self.add_camera)
 
         self._scene = QtWidgets.QGraphicsScene(self)
 
@@ -43,24 +50,21 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.cameraGraphicsView.setScene(self._scene)
 
         for k in self._cameras:
-            self.cameraComboBox.addItem(k[0])
+            self.cameraComboBox.addItem(k.address)
 
         self.addCameraPushButton.clicked.connect(self.add_camera)
 
-        self.camThread.update_video_source(self._cameras[0][0])
+        self.camThread.update_video_source(self._cameras[0].address)
         self.cameraComboBox.currentIndexChanged.connect(self.switch_camera)
-
 
     def resizeEvent(self, event: PySide6.QtGui.QResizeEvent) -> None:
         self.cameraGraphicsView.fitInView(self._pixmap_item, QtCore.Qt.KeepAspectRatio)
         super().resizeEvent(event)
 
-
     def set_image(self, image: QImage):
         pixmap = PySide6.QtGui.QPixmap.fromImage(image)
         self._pixmap_item.setPixmap(pixmap)
         self.cameraGraphicsView.fitInView(self._pixmap_item, QtCore.Qt.KeepAspectRatio)
-
 
     def add_camera(self):
         dlg = CameraAddWizard(self)
@@ -75,19 +79,24 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         print('square_polygon:', [p.toTuple() for p in square_polygon.toList()])
         print('roi_polygon:', [p.toTuple() for p in roi_polygon.toList()])
 
-        self._cameras.append((address, address))
+        self._cameras.append(Camera.from_wizard(
+            address,
+            float(square_length),
+            [p.toTuple() for p in square_polygon.toList()],
+            [p.toTuple() for p in roi_polygon.toList()]
+        ))
         self.cameraComboBox.addItem(address)
-        p
 
+    @QtCore.Slot(int)
+    def switch_camera(self, index: int):
+        self.camThread.update_video_source(self._cameras[index].address)
 
-@QtCore.Slot(int)
-def switch_camera(self, index: int):
-    self.camThread.update_video_source(self._cameras[index][0])
+    def closeEvent(self, event: PySide6.QtGui.QCloseEvent) -> None:
+        self.camThread.quit()
+        self.camThread.wait()
 
-
-def closeEvent(self, event: PySide6.QtGui.QCloseEvent) -> None:
-    self.camThread.quit()
-    self.camThread.wait()
+        with open('cameras2.json', 'w') as f:
+            json.dump([dataclasses.asdict(c) for c in self._cameras], f)
 
 
 if __name__ == '__main__':
