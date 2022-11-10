f3739057c7ae37ff598bde79422d607d46d5eda6
Maksim Pischulenok <pischule@outlook.com>
2022-11-11 00:04:04 +0300

    Delete project duplicate

--- assignments/РКСБП/Лаб DB/src/main/java/bsu/pischule/csab/imdb/data/repository/NameRepository.java
+++
@@ -1,8 +0,0 @@
-package bsu.pischule.csab.imdb.data.repository;
-
-import bsu.pischule.csab.imdb.data.entity.Name;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NameRepository extends JpaRepository<Name, Integer> {
-
-}
